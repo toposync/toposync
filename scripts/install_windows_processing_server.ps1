2493e01a717bb6baa1bea240b990da5e0eaee0f9
Mateus Calza <mateushenrique17@gmail.com>
2026-05-22 20:22:22 -0300

    chore(release): publish 0.4.24

--- scripts/install_windows_processing_server.ps1
+++ scripts/install_windows_processing_server.ps1
@@ -31,7 +31,7 @@ param(
     [ValidateSet("auto", "cpu", "directml", "cuda")]
     [string]$Bundle = "auto",
 
-    [string]$Version = "0.4.23",
+    [string]$Version = "0.4.24",
 
     [bool]$PreferLocalPackages = $true,
 

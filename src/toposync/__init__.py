d747b098ad9d17d1f08404b49e161ed38cda2a81
Mateus Calza <mateushenrique17@gmail.com>
2026-05-23 20:34:27 -0300

    chore(release): publish 0.5.2

--- src/toposync/__init__.py
+++ src/toposync/__init__.py
@@ -7,4 +7,4 @@ __all__ = ["__version__"]
 try:
     __version__ = importlib_metadata.version("toposync-core")
 except importlib_metadata.PackageNotFoundError:
-    __version__ = "0.5.1"
+    __version__ = "0.5.2"

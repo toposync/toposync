028fc851cf8ba6a1969490f44ed5f6a221873c50
Mateus Calza <mateushenrique17@gmail.com>
2026-06-05 13:17:15 -0300

    chore(release): publish 0.7.6

--- scripts/install_windows_processing_server.ps1
+++ scripts/install_windows_processing_server.ps1
@@ -31,7 +31,7 @@ param(
     [ValidateSet("auto", "cpu", "directml", "cuda")]
     [string]$Bundle = "auto",
 
-    [string]$Version = "0.7.5",
+    [string] = "0.7.6",
 
     [bool]$PreferLocalPackages = $true,
 
